ad9d1d04ea77aa808e6e84c512460ef4b3174ffd
Naimisha Gandham <82544897+naimisha5gandham@users.noreply.github.com>
2021-05-10 17:30:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+
+
+
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+const pool = mysql.createPool({
+    connectionLimit : 50,
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'nodejs_students'
+})
+
+
+
+
+app.get('', (req,res)=> {
+
+pool.getConnection((err,connection)=> {
+  if(err) throw err
+  console.log(`connected as id ${connection.threadId}`)
+  connection.query('SELECT * from students', (err,rows)=> {
+      connection.release()
+      if(!err){
+        res.send(rows)  
+      } else{
+          console.log(err)
+      }
+  })
+
+})
+
+
+})
+
+app.get('/:id', (req,res)=> {
+
+    pool.getConnection((err,connection)=> {
+      if(err) throw err
+      console.log(`connected as id ${connection.threadId}`)
+      connection.query('SELECT * from students WHERE id = ?', [req.params.id], (err,rows)=> {
+          connection.release()
+          if(!err){
+            res.send(rows)  
+          } else{
+              console.log(err)
+          }
+      })
+    
+    })
+    
+    
+    })
+
+    app.delete('/:id', (req,res)=> {
+
+        pool.getConnection((err,connection)=> {
+          if(err) throw err
+          console.log(`connected as id ${connection.threadId}`)
+          connection.query('DELETE  from students WHERE id = ?', [req.params.id], (err,rows)=> {
+              connection.release()
+              if(!err){
+                res.send(`Student with the Record ID : ${[req.params.id]} has been removed.`)  
+              } else{
+                  console.log(err)
+              }
+          })
+        
+        })
+        
+        
+        })
+     
+        app.post('', (req,res)=> {
+
+            pool.getConnection((err,connection)=> {
+              if(err) throw err
+              console.log(`connected as id ${connection.threadId}`)
+
+              const params = req.body
+
+              connection.query('INSERT  INTO students SET ?', params, (err,rows)=> {
+                  connection.release()
+                  if(!err){
+                    res.send(`Student with the name : ${params.name} has been added.`)  
+                  } else{
+                      console.log(err)
+                  }
+              })
+
+              console.log(req.body)
+            
+            })
+            
+            
+            })
+           
+
+            app.put('', (req,res)=> {
+
+                pool.getConnection((err,connection)=> {
+                  if(err) throw err
+                  console.log(`connected as id ${connection.threadId}`)
+    
+                  const params = req.body
+                  const { id, name, section, subject} = req.body
+    
+                  connection.query('UPDATE students SET name = ? , section = ? , subject = ? WHERE id = ?',[ name, section, subject,id] , (err,rows)=> {
+                      connection.release()
+                      if(!err){
+                        res.send(`Student with the name : ${name} has been updated.`)  
+                      } else{
+                          console.log(err)
+                      }
+                  })
+    
+                  console.log(req.body)
+                
+                })
+                
+                
+                })
+
+app.listen(port, ()=> console.log(`Listen on port ${port}`))
